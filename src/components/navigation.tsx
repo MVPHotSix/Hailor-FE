73d17679dbfb027929684e863ae3fedc8e301ae8
123isi <123isi@naver.com>
2025-02-12 18:50:45 +0900

    feat :: navgation bar추가

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,17 +1,44 @@
 import styled from 'styled-components'
 
-const NavigationLayout = styled.div`
-    display: grid;
-    width: 100%;
-`
+import calender from '../assets/calender.svg'
+import chat from '../assets/chat.svg'
+import home from '../assets/home.svg'
+import search from '../assets/search.svg'
+import profile from '../assets/profile.svg'
 
 function Navigation() {
-    // TODO: implement content
     return (
         <NavigationLayout>
-            <span>navigation</span>
+            <Tie><Img src={home} alt="홈" />홈</Tie>
+            <Tie><Img src={search} alt="검색" />검색</Tie>
+            <Tie><Img src={calender} alt="예약" />예약</Tie>
+            <Tie><Img src={chat} alt="채팅" />채팅</Tie>
+            <Tie><Img src={profile} alt="마이" />마이</Tie>
         </NavigationLayout>
     )
 }
 
+const NavigationLayout = styled.div`
+    display: flex;
+
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    gap: 49.5px;
+    margin-bottom: 34px;
+`
+
+
+const Img = styled.img`
+    width: 24px;
+    height: 24px;
+
+`
+const Tie = styled.div`
+    width: 24px;
+    height: 49px;
+    font-size:12px;
+
+
+`
 export default Navigation
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,25 +2,38 @@ import Header from '../components/header.tsx'
 import Navigation from '../components/navigation.tsx'
 import styled from 'styled-components'
 
-const MainLayout = styled.div`
-    display: grid;
+const Body = styled.div`
+    display: flex;
+    justify-content: center;  
+    align-items: center;      
     width: 100vw;
     height: 100vh;
+
+`
+
+const MainLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 375px;
+    height: 100vh;
+    background-color: white;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 `
 
 const ContentLayout = styled.div`
-    width: 100%;
-    height: 100%;
+    flex: 1; 
     overflow-y: scroll;
 `
 
 function Main() {
     return (
-        <MainLayout>
-            <Header />
-            <ContentLayout>content</ContentLayout>
-            <Navigation />
-        </MainLayout>
+        <Body>
+            <MainLayout>
+                <Header />
+                <ContentLayout>content</ContentLayout>
+                <Navigation />
+            </MainLayout>
+        </Body>
     )
 }
 
