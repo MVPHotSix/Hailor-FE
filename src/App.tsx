35677a1ca91930f6ff91c38b81884491ff7318e6
Yoon Jaehyun <yuchem2@gmail.com>
2025-02-17 17:10:43 +0900

    chore: make .env file logic in ci/cd scripts

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import PaymentFailure from './pages/payment/paymentFailure.tsx'
 import PaymentSuccess from './pages/payment/paymentSuccess.tsx'
 import PgServer from './pages/payment/pgServer.tsx'
 import NotFound from './pages/notFound.tsx'
+import { VITE_SEVER_URL } from './config'
 
 const Layout = styled.div`
     width: 100vw;
@@ -24,6 +25,7 @@ const Layout = styled.div`
 `
 
 function App() {
+    console.log(VITE_SEVER_URL)
     const [queryClient] = useState(
         () =>
             new QueryClient({
